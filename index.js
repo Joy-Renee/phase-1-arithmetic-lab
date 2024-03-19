e6c5d66e4b4e8c1e68906d96faf8463aa2dc78a7
Joy Renee Mugo <mugojoy24@gmail.com>
2024-03-20 01:06:18 +0300

    New file

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+
+//random
+let random = 5
+Math.random() 
+
+//mod
+let num3 = 28; 
+let num4 = 6;
+let mod = num3 % num4;
+
+//max
+let max = Math.max(15,10,2,19,20,8);
